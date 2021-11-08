af38e644c1dc310f530242e29866aa0c6a77043e
Red Oral <redmunozoral@gmail.com>
2021-11-08 12:25:10 -0800

    Styling tweaks to the header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,11 @@ import DownArrowIcon from '../assets/down-arrow.png';
 const HeaderComponent: React.FC = () => {
   return (
     <div className='header-container'>
-      <h1>Hi! I'm Red. 👋</h1>
-      <p>I'm a JavaScript Developer. Scroll below to see more about me!</p>
+      <h1 id='header-text'>Hi, I'm Red! ✌️</h1>
+      <p id='header-subtext'>
+        I'm a JavaScript developer currently based in Fairfield, CA.
+        <br /> Scroll down to see more about me and my work!
+      </p>
       <img src={DownArrowIcon} width='50' height='50' id='down-arrow-icon' />
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,36 +2,36 @@ import React from 'react';
 import HamburgerComponent from './Hamburger';
 
 const NavbarComponent: React.FC = () => {
-	const [visible, setVisible] = React.useState(false);
+  const [visible, setVisible] = React.useState(false);
 
-	return (
-		<>
-			<div className='navbar-container'>
-				<h2>{"red's portfolio."}</h2>
-				<ul className='navbar'>
-					<li>
-						<a href='#'>Skills</a>
-					</li>
-					<li>
-						<a href='#'>Projects</a>
-					</li>
-					<li>
-						<a href='#'>Contact</a>
-					</li>
-				</ul>
-				<div className='hamburger-nav'>
-					<a
-						href='#'
-						className='hamburger-icon'
-						onClick={() => (!visible ? setVisible(true) : setVisible(false))}
-					>
-						≡
-					</a>
-				</div>
-			</div>
-			<HamburgerComponent visible={visible} setVisible={setVisible} />
-		</>
-	);
+  return (
+    <>
+      <div className='navbar-container'>
+        <h2 id='site-logo'>RDRL</h2>
+        <ul className='navbar'>
+          <li>
+            <a href='#'>About</a>
+          </li>
+          <li>
+            <a href='#'>Projects</a>
+          </li>
+          <li>
+            <a href='#'>Contact</a>
+          </li>
+        </ul>
+        <div className='hamburger-nav'>
+          <a
+            href='#'
+            className='hamburger-icon'
+            onClick={() => (!visible ? setVisible(true) : setVisible(false))}
+          >
+            ≡
+          </a>
+        </div>
+      </div>
+      <HamburgerComponent visible={visible} setVisible={setVisible} />
+    </>
+  );
 };
 
 export default NavbarComponent;
