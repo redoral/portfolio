ab19c8c03f245770331033f8be60fe3f43b2c53d
Red Oral <redmunozoral@gmail.com>
2021-12-10 13:13:42 -0800

    Replaced google maps embed with a selfie lol

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import me from '../assets/me.jpg';
 
 const ContactComponent: React.FC = () => {
   const [subject, setSubject] = React.useState('');
@@ -33,13 +34,7 @@ const ContactComponent: React.FC = () => {
           isVisible ? 'is-visible' : ''
         }`}
       >
-        <iframe
-          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100284.04764086362!2d-122.10115367941367!3d38.23597327748718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808514d0f123c8db%3A0x5c335616d29bf368!2sFairfield%2C%20CA!5e0!3m2!1sen!2sus!4v1637191238642!5m2!1sen!2sus'
-          width='600'
-          height='475'
-          loading='lazy'
-          className='google-maps-embed '
-        ></iframe>
+        <div className='me-img' />
         <div className='contact-box'>
           <h1 className='section-title-text' ref={contactRef}>
             Contact me!
