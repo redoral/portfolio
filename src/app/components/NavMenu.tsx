d03e8d20dc9573676ac355c63afcb26d42deb076
Red Oral <redmunozoral@gmail.com>
2024-11-30 11:35:03 -0800

    removed projects, updated homepage and icon

--- src/app/components/NavMenu.tsx
+++ src/app/components/NavMenu.tsx
@@ -8,17 +8,12 @@ function NavMenu() {
 
   return (
     <nav className='col-span-1'>
-      <Image src='/img/logos/redoral.png' width={32} height={32} alt='Site logo' className='mx-2 my-8' />
+      <Image src='/img/logos/logo.png' width={32} height={32} alt='Site logo' className='mx-2 mb-8 mt-4' />
       <ul className='flex flex-col gap-2'>
         <li
           className={`w-32 p-2 transition-colors hover:text-white ${pathname === '/' ? 'rounded bg-white bg-opacity-10 font-semibold' : 'text-gray-300'} text-md`}
         >
           <Link href='/'>Home</Link>
-        </li>
-        <li
-          className={`w-32 p-2 transition-colors hover:text-white ${pathname === '/projects' ? 'rounded bg-white bg-opacity-10 font-semibold text-white' : 'text-gray-300'} text-md`}
-        >
-          <Link href='/projects'>Projects</Link>
         </li>
         <li
           className={`w-32 p-2 transition-colors hover:text-white ${pathname === '/contact' ? 'rounded bg-white bg-opacity-10 font-semibold' : 'text-gray-300'} text-md`}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,7 +5,7 @@ export default async function Page() {
   return (
     <PageLayout>
       <h1 className='text-4xl font-bold'>Contact</h1>
-      <p className='text-gray-300'>Currently not taking email, text, or calls.</p>
+      <p className='text-gray-300'>Currently not taking emails, texts, or calls.</p>
       <p className='text-gray-300'>
         For inquiries, you can message me on{' '}
         <a href='https://linkedin.com/in/redoral' target='_blank' className='text-white underline'>
