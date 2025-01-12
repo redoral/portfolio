673d263b2f31ea18e08ff165a382c9f4b97b6b81
redoral <redmunozoral@gmail.com>
2025-01-11 22:18:55 -0800

    updated mobile nav, added custom 404 page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import React from 'react';
+import { MdNoCell, MdOutlinePhone, MdOutlinePhoneDisabled } from 'react-icons/md';
 
 export const metadata: Metadata = {
   title: 'Contact | Red Oral',
@@ -7,12 +8,17 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   return (
-    <div className='col-span-1 flex w-full flex-col gap-4 pb-12 lg:col-span-3'>
+    <div className='col-span-1 flex w-full flex-col gap-6 pb-12 lg:col-span-3'>
       <h1 className='text-4xl font-bold'>Contact</h1>
+      <MdOutlinePhoneDisabled className='my-12 text-red-300' size={128} />
       <p className='text-gray-300'>Currently not taking emails, texts, or calls.</p>
       <p className='text-gray-300'>
         For inquiries, you can message me on{' '}
-        <a href='https://linkedin.com/in/redoral' target='_blank' className='text-white underline'>
+        <a
+          href='https://linkedin.com/in/redoral'
+          target='_blank'
+          className='underline transition-colors hover:text-white'
+        >
           LinkedIn
         </a>
         .
---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { TbError404Off } from 'react-icons/tb';
+
+function NotFoundPage() {
+  return (
+    <div className='col-span-1 flex w-full flex-col gap-6 pb-12 text-gray-300 lg:col-span-3'>
+      <h1 className='text-4xl font-bold'>Not found</h1>
+      <TbError404Off className='my-12 text-red-300' size={128} />
+      <p>The page you are looking for does not exist.</p>
+      <a
+        href='https://linkedin.com/in/redoral'
+        target='_blank'
+        className='underline transition-colors hover:text-white'
+      >
+        Return home
+      </a>
+    </div>
+  );
+}
+
+export default NotFoundPage;
